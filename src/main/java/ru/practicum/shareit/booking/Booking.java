0a7416fd1768ebd2f63cd139af1ad4d6a08b4326
farma <krikunovalexandr1990@yandex.ru>
2025-08-23 22:01:04 +0500

    создать модели

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class Booking {
+    private long bookingId;
+    private LocalDateTime startBooking;
+    private LocalDateTime endBooking;
+    private Item item;
+    private User booker;
+    private StatusBooking bookingStatus;
 }
---
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.item;
+
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@Data
+public class Item {
+    private long itemID;
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+    private ItemRequest request;
+}
